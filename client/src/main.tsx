66e024b28e8dc8bc0bfebb432e78771e895dd614
S-Hov <shov7277@gmail.com>
2026-07-30 14:34:49 +0300

    Connect AuthProvider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,10 +7,13 @@ import { router } from './app/routing/router'
 import './index.css'
 import './app/styles/global.css'
 import './app/styles/variables.css'
+import { AuthProvider } from './app/providers/auth/AuthProvider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Toaster richColors position="top-center" closeButton />
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
