530133372fa320ce5a4dc517aa48b1050aa19cb9
S-Hov <shov7277@gmail.com>
2026-08-04 13:24:10 +0300

    Add bootstrap provider

---
+++ client/src/app/providers/bootstrap/BootstrapProvider.tsx
@@ -0,0 +1,41 @@
+import { useCallback, useEffect, useState, type ReactNode } from "react";
+import { BootstrapContext, type BootstrapState } from "./BootstrapContext";
+import { bootstrap } from "../../../shared/api/bootstrap";
+
+interface BootstrapProviderProps {
+    children: ReactNode;
+}
+
+export const BootstrapProvider = ({ children }: BootstrapProviderProps ) => {
+    const [state, setState] = useState<BootstrapState>({
+        status: 'checking',
+        stage: null
+    });
+
+    const refreshBootstrap = useCallback(async (): Promise<void> => {
+        setState({ status: 'checking', stage: null });
+
+        try {
+            const response = await bootstrap.getStatus();
+            
+            if (!response.data) {
+                throw new Error('Сервер не вернул данные о статусе bootstrap');
+            }
+
+            setState({ status: 'resolved', stage: response.data.stage });
+        }
+        catch (error: unknown) {
+            setState({ status: 'error', stage: null });
+        }
+    }, []);
+
+    useEffect(() => {
+        void refreshBootstrap();
+    }, [refreshBootstrap]);
+
+    return (
+        <BootstrapContext.Provider value={{ state, refreshBootstrap }}>
+            {children}
+        </BootstrapContext.Provider>
+    )
+}
