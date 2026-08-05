b54b3278d80f860dbc1dd218c98e19fec0ef0374
S-Hov <shov7277@gmail.com>
2026-08-05 16:25:35 +0300

    Auto transitions between pages

--- client/src/app/routing/guards/BootstrapGate.tsx
+++ client/src/app/routing/guards/BootstrapGate.tsx
@@ -1,10 +1,12 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, Navigate } from "react-router-dom";
 import { useBootstrap } from "../../providers/bootstrap/BootstrapContext";
 import { Button } from "../../../shared/ui/Button/Button";
 
 
 export const BootstrapGate = () => {
     const { state, refreshBootstrap } = useBootstrap();
+    const location = useLocation();
+
     if (state.status === 'checking') {
         return <div>Проверяем статус bootstrap...</div>;
     }
@@ -23,5 +25,45 @@ export const BootstrapGate = () => {
         );
     }
 
+    if (state.stage === 'database_required' && location.pathname !== '/install') {
+        return <Navigate to="/install" />;
+    }
+
+    if (state.stage === 'migrations_required' && location.pathname !== '/install/runMigrations') {
+        return <Navigate to="/install/runMigrations" />;
+    }
+
+    if (state.stage === 'admin_required' && location.pathname !== '/install/register') {
+        return <Navigate to="/install/register" />;
+    }
+
+    if (state.stage === 'database_unavailable') {
+        return (
+            <div>
+                База данных недоступна.
+                <Button
+                    variant="primary"
+                    onClick={refreshBootstrap}
+                >
+                    Проверить повторно
+                </Button>
+            </div>
+        );
+    }
+
+    else if (state.stage === 'system_error') {
+        return (
+            <div>
+                Произошла системная ошибка.
+                <Button
+                    variant="primary"
+                    onClick={refreshBootstrap}
+                >
+                    Проверить повторно
+                </Button>
+            </div>
+        );
+    }
+    
     return <Outlet />;
 }
