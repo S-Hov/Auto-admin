c883c7ae8eada8a5cd8d7fe8ef94ff0599a418f6
S-Hov <shov7277@gmail.com>
2026-08-20 21:22:31 +0300

    refactor: replace custom conflict error with standard Error in requirePendingMigrations middleware

--- server/src/shared/middleware/requirePendingMigrations.ts
+++ server/src/shared/middleware/requirePendingMigrations.ts
@@ -1,14 +1,13 @@
 import type { NextFunction, Request, Response } from "express";
 import { getCurrentMigrationPlan } from "../../migrations/migration.runner";
-import { conflict } from "../api/errors/error-helpers";
 
 export const requirePendingMigrations = async (_req: Request, _res: Response, next: NextFunction) => {
     try {
         const plan = await getCurrentMigrationPlan();
-    
+
         if (!plan.isComplete) return next();
 
-        return next(conflict('Миграции уже установлены', undefined, 'INSTALL.MIGRATIONS_ALREADY_COMPLETED'));
+        return next(new Error('Миграции уже установлены'));
     }
     catch (error) {
         return next(error);
