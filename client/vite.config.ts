b222ec8a3b788c557f51570584d5dc3ef1aef2e3
S-Hov <shov7277@gmail.com>
2026-08-21 23:15:58 +0300

    chore: optimize docker configurations by adding named volumes for node_modules, implementing .dockerignore files, and securing container user permissions

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // слушать все интерфейсы внутри контейнера
+    port: 5173,
+    watch: {
+      usePolling: true, // стабильный HMR внутри Docker / WSL
+    },
+  },
 })
