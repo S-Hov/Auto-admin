442d9a7e22c771817d3ff186b1dda16936863d3b
S-Hov <shov7277@gmail.com>
2026-08-23 17:26:04 +0300

    refactor: remove unused dotenv import from express application entry point

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
 import { logger } from './shared/middleware/logger';
 import { errorHandler } from './shared/middleware/errorHandler';
 import cookieParser from 'cookie-parser';
