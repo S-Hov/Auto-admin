064b36b5264bf5aa7b513246337fe7151f5db7c1
S-Hov <shov7277@gmail.com>
2026-08-26 17:00:14 +0300

    feat: implement batch query execution support using transactions in QueryEngineService

--- server/src/core/query-engine/query-engine.service.ts
+++ server/src/core/query-engine/query-engine.service.ts
@@ -1,14 +1,33 @@
+import { getPool, withTransaction } from "../../db";
 import { MySqlCompiler } from "./compiler/mysql.compiler";
 import type { DatabaseDriver, QueryResult } from "./drivers/driver.types";
 import { MySqlDriver } from "./drivers/mysql.driver";
 import type { UnifiedQuery } from "./types/query.types";
 
 export class QueryEngineService {
-    constructor(private driver: DatabaseDriver = new MySqlDriver()) {}
+    constructor(private driver: DatabaseDriver = new MySqlDriver()) { }
 
-    async execute<T = unknown>(query: UnifiedQuery): Promise<QueryResult<T>> {
-        const compiled = MySqlCompiler.compile(query);
-        return await this.driver.execute<T>(compiled);
+    async execute<T = unknown>(query: UnifiedQuery): Promise<QueryResult<T>>;
+
+    async execute<T = unknown>(query: UnifiedQuery[]): Promise<QueryResult<T>[]>;
+
+    async execute<T = unknown>(query: UnifiedQuery | UnifiedQuery[]): Promise<QueryResult<T> | QueryResult<T>[]> {
+        if (Array.isArray(query)) {
+            return await withTransaction(async (transaction) => {
+                const result: QueryResult<T>[] = [];
+                const txDriver = new MySqlDriver(transaction);
+                for (const c of query) {
+                    const compiled = MySqlCompiler.compile(c);
+                    result.push(await txDriver.execute<T>(compiled))
+                }
+                return result;
+            })
+        }
+        else {
+            this.driver = new MySqlDriver(getPool());
+            const compiled = MySqlCompiler.compile(query);
+            return await this.driver.execute<T>(compiled);
+        }
     }
 
     async ping(): Promise<boolean> {
