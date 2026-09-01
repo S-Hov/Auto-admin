aa0eb59d84b6e2bd5e45966990528e48565fe211
S-Hov <shov7277@gmail.com>
2026-09-01 16:21:21 +0300

    refactor: update bootstrap status controller to use success code constant

--- server/src/modules/bootstrap/bootstrap.controller.ts
+++ server/src/modules/bootstrap/bootstrap.controller.ts
@@ -3,13 +3,14 @@ import { ok } from '../../shared/api/success';
 import { asyncHandler } from '../../utils/asyncHandler';
 import { getBootstrapStatusService } from "./bootstrap.service";
 import { BootstrapStatusResponse } from "./bootstrap.types";
+import { SUCCESS_CODES } from "../../shared/api/codes/success-codes";
 
 export const getBootstrapStatusController = asyncHandler(async (_req: Request, res: Response) => {
     const stage = await getBootstrapStatusService();
 
     return ok<BootstrapStatusResponse>(
         res,
-        'Состояние приложения определено',
+        SUCCESS_CODES.BOOTSTRAP_STATUS_RECEIVED,
         { stage }
     );
 })
