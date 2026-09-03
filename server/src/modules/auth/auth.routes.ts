2d32a321352fb4de53c2edceb941c8b257f7087e
S-Hov <shov7277@gmail.com>
2026-09-03 12:45:50 +0300

    feat: add rate limiter to login route

--- server/src/modules/auth/auth.routes.ts
+++ server/src/modules/auth/auth.routes.ts
@@ -3,10 +3,11 @@ import { getMeController, loginController, logoutController } from "./auth.contr
 import { requireAuth } from "../../shared/middleware/auth.middleware";
 import { validate } from "../../shared/middleware/validate";
 import { loginSchema } from "./schema/login.schema";
+import { rateLimiter } from "../../shared/middleware/rateLimiter";
 
 const authRouter = express.Router();
 
-authRouter.post('/login', validate(loginSchema), loginController);
+authRouter.post('/login', rateLimiter, validate(loginSchema), loginController);
 authRouter.get('/me', requireAuth, getMeController);
 authRouter.post('/logout', logoutController);
 
