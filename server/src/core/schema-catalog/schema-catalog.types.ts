e62c909753bfc111d6800fae6ab66da2aadf4c84
S-Hov <shov7277@gmail.com>
2026-09-08 22:19:43 +0300

    feat: refactor schema catalog types to include detailed constraint definitions and column metadata structures

--- server/src/core/schema-catalog/schema-catalog.types.ts
+++ server/src/core/schema-catalog/schema-catalog.types.ts
@@ -4,27 +4,56 @@ export interface DBSnapshot {
     tables: DBTable[];
 }
 
+export interface DBPrimary {
+    name: "PRIMARY";
+    columns: string[];
+}
+
+export interface DBUniq {
+    name: string;
+    columns: string[];
+}
+
+export type ForeignKeyAction = 'CASCADE' | 'SET NULL' | 'RESTRICT' | 'NO ACTION' | 'SET DEFAULT';
+
+export interface DBForeign {
+    name: string;
+    columns: string[];
+    refTableName: string;
+    refColumns: string[];
+    onUpdate: ForeignKeyAction;
+    onDelete: ForeignKeyAction;
+}
+
 export interface DBTable {
     name: string;
     type: 'table' | 'view';
-    speakers: DBSpeaker[];
-    primaryKey: string;
-    uniqKeys: string[];
-    foreignKeys: string[];
+    columns: DBColumn[];
+    primaryKey: DBPrimary | null;
+    uniqueKeys: DBUniq[];
+    foreignKeys: DBForeign[];
 }
 
-export interface DBSpeaker {
+export interface DBGenerated {
+    isGenerated: boolean;
+    generationExpression: string | null;
+}
+
+export interface DBColumn {
     name: string;
     position: number;
+    dataType: string;
     columnType: string;
     nullable: boolean;
-    default: unknown;
-    generated: Date;
+    default: string | null;
+    generated: DBGenerated;
     autoIncrement: boolean;
 }
 
 export interface DBUnion {
     name: string;
-    speakers: DBSpeaker[];
-    type: 'CASCADE' | 'SET NULL' | 'RESTRICT' | 'NO ACTION' | 'SET DEFAULT'
+    columns: Extract<DBColumn, 'name'>[];
+    onDelete: ForeignKeyAction;
+    onUpdate: ForeignKeyAction;
+    type: 'LEFT' | 'RIGHT' | 'INNER' | 'OUTER';
 }
