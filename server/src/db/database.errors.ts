0a67985b8b5d9e09a9adabc07868e76b52206e5b
S-Hov <shov7277@gmail.com>
2026-09-21 16:01:59 +0300

    feat: add status and code properties to UnsupportedDatabaseError and introduce UnsupportedDatabaseSubsystemError

--- server/src/db/database.errors.ts
+++ server/src/db/database.errors.ts
@@ -1,5 +1,21 @@
 export class UnsupportedDatabaseError extends Error {
+    readonly status: number;
+    readonly code: string;
+
     constructor(databaseName: string) {
         super(`Database ${databaseName} is not supported`);
-    }    
+        this.status = 501;
+        this.code = 'UNSUPPORTED_DATABASE';
+    }
+}
+
+export class UnsupportedDatabaseSubsystemError extends Error {
+    readonly status: number;
+    readonly code: string;
+
+    constructor(subsystem: string) {
+        super(`Database subsystem ${subsystem} is not supported`);
+        this.status = 501;
+        this.code = 'UNSUPPORTED_DATABASE_SUBSYSTEM';
+    }
 }
