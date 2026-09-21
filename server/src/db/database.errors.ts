c30c2e9cdace0652c56a622043359b0d1e570970
S-Hov <shov7277@gmail.com>
2026-09-21 21:42:51 +0300

    refactor(db): simplify error class constructors and properties

--- server/src/db/database.errors.ts
+++ server/src/db/database.errors.ts
@@ -1,39 +1,26 @@
 import { ERROR_CODES } from "../shared/api/codes/error-codes";
-import type { DatabaseType } from "./database.types";
+import type { DatabaseSubsystem, DatabaseType } from "./database.types";
 
 export class UnsupportedDatabaseError extends Error {
-    public readonly databaseType: DatabaseType;
-    readonly status: number;
-    readonly code: string;
-
-    constructor(databaseType: DatabaseType) {
+    constructor(public readonly databaseType: DatabaseType) {
         super(`Database ${databaseType} is not supported`);
-        this.databaseType = databaseType;
-        this.status = 501;
-        this.code = ERROR_CODES.UNSUPPORTED_DATABASE;
+        this.name = "UnsupportedDatabaseError";
     }
 }
 
 export class UnsupportedDatabaseSubsystemError extends Error {
-    readonly databaseType: DatabaseType;
-    readonly subsystem: string;
-    readonly status: number;
-    readonly code: string;
-
-    constructor(databaseType: DatabaseType, subsystem: string) {
-        super(`Database ${databaseType} subsystem ${subsystem} is not supported`);
-        this.databaseType = databaseType;
-        this.subsystem = subsystem;
-        this.status = 501;
-        this.code = ERROR_CODES.UNSUPPORTED_DATABASE_SUBSYSTEM;
+    constructor(
+        public readonly databaseType: DatabaseType,
+        public readonly databaseSubsystem: DatabaseSubsystem
+    ) {
+        super(`Database subsystem ${databaseSubsystem} is not supported for database ${databaseType}`);
+        this.name = "UnsupportedDatabaseSubsystemError";
     }
 }
 
 export class DatabaseProviderNotFoundError extends Error {
-    public readonly databaseType: DatabaseType;
-
-    constructor(databaseType: DatabaseType) {
+    constructor(public readonly databaseType: DatabaseType) {
         super(`Database provider for ${databaseType} is not registered`);
-        this.databaseType = databaseType;
+        this.name = "DatabaseProviderNotFoundError";
     }
 }
