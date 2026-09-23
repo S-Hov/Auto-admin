a8c59634ad8963ce208d4da07e54dfe8524078a3
S-Hov <shov7277@gmail.com>
2026-09-23 22:20:05 +0300

    feat(db): add database connection configuration types

---
+++ server/src/db/database-connection.types.ts
@@ -0,0 +1,38 @@
+import type { DatabaseType } from "./database.types";
+
+export interface NetworkDatabaseConnectionConfig {
+    host: string;
+    port: number;
+    database: string;
+    user: string;
+    password: string;
+}
+
+export interface MySqlDatabaseConnectionConfig extends NetworkDatabaseConnectionConfig {
+    type: "mysql";
+}
+
+export interface PostgreSqlDatabaseConnectionConfig extends NetworkDatabaseConnectionConfig {
+    type: "postgresql";
+}
+
+export interface SqliteDatabaseConnectionConfig {
+    type: "sqlite";
+    filePath: string;
+}
+
+type AllConfigs =
+    | MySqlDatabaseConnectionConfig
+    | PostgreSqlDatabaseConnectionConfig
+    | SqliteDatabaseConnectionConfig;
+
+export type DatabaseConnectionConfig<T extends DatabaseType = DatabaseType> =
+    Extract<AllConfigs, { type: T }>;
+
+export type DatabaseConnectionConfigMap = {
+    [K in DatabaseType]: Extract<AllConfigs, { type: K }>;
+};
+
+export interface DatabaseConnectionCheckResult {
+    version: string;
+}
