fa0735de93d25562d2d8ad13c971bbdaec716ef7
S-Hov <shov7277@gmail.com>
2026-09-25 22:10:35 +0300

    refactor(query-engine-provider): make interface fields readonly and constrain database type

--- server/src/core/query-engine/providers/query-engine-provider.interface.ts
+++ server/src/core/query-engine/providers/query-engine-provider.interface.ts
@@ -1,9 +1,12 @@
+import type { DatabaseType } from "../../../db/contracts/database.types";
 import type { DatabaseExecutor } from "../../../db/contracts/executor.interface";
 import type { QueryCompiler } from "../compiler/query-compiler.interface";
 import type { DatabaseDriver } from "../drivers/driver.types";
 
-export interface QueryEngineProvider<TDatabaseType> {
-    type: TDatabaseType;
-    compiler: QueryCompiler;
+export interface QueryEngineProvider<
+    TDatabaseType extends DatabaseType = DatabaseType,
+> {
+    readonly type: TDatabaseType;
+    readonly compiler: QueryCompiler;
     createDriver(executor: DatabaseExecutor): DatabaseDriver;
 }
